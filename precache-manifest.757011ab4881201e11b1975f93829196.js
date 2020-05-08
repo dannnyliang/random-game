794621b887fa00a161050915f24e32588f07813b
dannnyliang <youchenliang@gmail.com>
2020-05-09 00:15:51 +0800

    Updates

---
+++ precache-manifest.757011ab4881201e11b1975f93829196.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcf78015fbd46dc983c1e3292d570b2f",
+    "url": "/random-game/index.html"
+  },
+  {
+    "revision": "405d645af64b471b1553",
+    "url": "/random-game/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2ec35bf456164e0e55f6",
+    "url": "/random-game/static/js/2.b530713f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/random-game/static/js/2.b530713f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "405d645af64b471b1553",
+    "url": "/random-game/static/js/main.a2710781.chunk.js"
+  },
+  {
+    "revision": "ef684a6a00d96c96cc95",
+    "url": "/random-game/static/js/runtime-main.17d99137.js"
+  }
+]);
